f06e71b89b7459fbbb2a6c33b0bf0ec46321e12c
PuneetSingla1234 <puneet2singla@gmail.com>
2020-05-04 20:20:12 +0530

    Modified Assignment By Adding Service Layer

--- src/main/java/guru/springframework/sfgjokesapp/controllers/JokesController.java
+++ src/main/java/guru/springframework/sfgjokesapp/controllers/JokesController.java
@@ -1,16 +1,20 @@
 package guru.springframework.sfgjokesapp.controllers;
 
+import guru.springframework.sfgjokesapp.services.JokesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
+    private final JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
 
     @RequestMapping({"/",""})
     public String getJoke(Model model){
-        String joke=new guru.springframework.norris.chuck.ChuckNorrisQuotes().getRandomQuote();
-        model.addAttribute("joke",joke);
-        return "chucknorris";
+        return jokesService.getJoke();
     }
 }
